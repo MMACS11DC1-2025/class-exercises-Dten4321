55bde54d85b65e8f3aadf460d3beae5b9d967561
Dten4321 <shalena.ca@gmail.com>
2025-09-26 14:39:53 -0700

    did 2.4

--- 2.4/main.py
+++ 2.4/main.py
@@ -1,13 +1,12 @@
 file = open("2.4/responses.csv")
 
 junk = file.readline()
+lines = file.read().split("\n")
 
-answers = file.read().strip("\n")
-
-for answer in answers:
-    
-    line = file.readline().strip()
-
-print(answers[1])
-
-file = open("2.4/responses.csv")
+for line in lines:
+    data = line.split(",")
+    if "Derick Su" in data:
+        print("I found Derick Su")
+        break
+    else:
+        print(f"{data[1]} is not Derick Su!")
