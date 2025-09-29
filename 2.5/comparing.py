759cd3b2f04641de30ccac42e6e01090b9293979
Dten4321 <shalena.ca@gmail.com>
2025-09-29 15:05:48 -0700

    Continued to finish comparing assignment

--- 2.5/comparing.py
+++ 2.5/comparing.py
@@ -11,39 +11,49 @@ file = open("2.4/responses.csv")
 
 junk = file.readline()
 
-name1 = input("Please enter a your name: ")
+name1 = input("Please enter a name: ").lower().strip()
 
-data = file.read().split("\n")
+data = file.read().strip().split("\n")
 
 similarity = []
+similarityName = []
 
 yourIndex = 0
 
 for line in data:
     datas = line.split(",")
-    if name1 in datas:
+    if name1 == datas[1].lower():
         yourData = datas
-        print(yourData)
         break
     yourIndex += 1
 
-index = 0
-
 for line in data:
-    if yourIndex != index:
-        similarity.append(0)
-    index += 1
+    similarity.append(0)
+    similarityName.append(0)
 
 index = 0
 for line in data:
     datas = line.split(",")
-    if index != yourIndex:
-        i = 0
-        for perameters in datas:
-            if yourData[i] == datas[i]:
-                similarity[index] += 1
-                print(similarity[index])
-        print(f"You have {similarity[index]} similarities with {datas[1]}")
+    #if index != yourIndex:
+    i = 0
+    for perameters in datas:
+        if yourData[i] == datas[i]:
+            similarity[index] += 1
+            similarityName[index] = datas[1]
+            #print(similarity[index])
+        i += 1
+    #print(f"{name1} has {similarity[index]} similarities with {datas[1]}")
     index += 1
-    
+
+maxSimilarity = 0
+for i in range(len(similarity)):
+    if i != yourIndex:
+        if similarity[i] > maxSimilarity:
+            maxSimilarity = similarity[i]
+#print(maxSimilarity)
+
+for i in range(len(similarity)):
+    if similarity[i] == maxSimilarity:
+        print(f"{similarityName[i]} has most similarities with {name1} with {similarityName[yourIndex]} similarities.")
+
     
