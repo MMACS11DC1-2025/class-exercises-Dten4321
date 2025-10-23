aa168a71f57bfc1098ca4d84aa699c6c4f6d9f65
Dten4321 <shalena.ca@gmail.com>
2025-10-23 14:12:47 -0700

    did homework

--- 4.1/evolution simulator.py
+++ 4.1/evolution simulator.py
@@ -14,6 +14,7 @@
 
 import turtle
 import random
+import time
 
 t = turtle.Turtle()
 t.speed(0)
@@ -22,7 +23,7 @@ t.hideturtle()
 turtle.delay(0)
 turtle.tracer(0)
 lifeSide = 0
-maxTime = 250
+maxTime = 5
 timer = maxTime
 class Ball:
     def __init__(self, speed, timer, direction, pos):
@@ -32,6 +33,7 @@ class Ball:
         self.pos = pos
         self.size = 20 - speed
         self.time = 0
+        self.turnedBefore = False
     def getStuff(self):
         return [self.speed, self.timer, self.direction, self.pos, self.size]
     
@@ -47,11 +49,19 @@ class Ball:
         self.pos[1] += self.speed * self.direction
         
     def timeDo(self):
-        if self.time >= self.timer:
-            self.direction = -(self.direction)
-            self.time = 0
-        else: 
-            self.time += 1
+        if self.turnedBefore:
+            if self.time > self.timer:
+                self.direction = -(self.direction)
+                self.time = 0
+            else: 
+                self.time += 1
+        elif not self.turnedBefore:
+            if self.time > int(self.timer/2):
+                self.direction = -(self.direction)
+                self.time = 0
+                self.turnedBefore = True
+            else: 
+                self.time += 1
     
     def checkLife(self):
         if lifeSide == 1:
@@ -60,27 +70,25 @@ class Ball:
         elif lifeSide == 2:
             if self.pos[1] < 0:
                 return False
-        else:
-            return True
+        return True
 
 balls = []
 
 
 
-for i in range(20):
-    balls.append(Ball(random.randrange(1, 5),random.randrange(300),random.choice([-1, 1]), [int(random.randrange(-350, 350)), int(random.randrange(-100, 100))]))
-    
+for i in range(100):
+    balls.append(Ball(random.randrange(1, 5),random.randrange(300),random.choice([-1, 1]), [int(random.randrange(-500, 500)), 0]))
+
+lastRuntime = time.process_time()
+
 while True:
-    print(timer)
-    if timer > 0:
-        timer -= 1
-    else:
+    if time.process_time() - lastRuntime > maxTime:
+        lastRuntime = time.process_time()
         if lifeSide == 1:
             lifeSide = 2
-            timer = maxTime
         elif lifeSide == 2 or lifeSide == 0:
             lifeSide = 1
-            timer = maxTime
+    
     t.clear()
     for ball in balls:
         ball.draw()
