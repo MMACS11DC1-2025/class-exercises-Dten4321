6519cb2fb72dbfb587988699e591293266ba0aa5
Dten4321 <shalena.ca@gmail.com>
2025-11-12 13:27:33 -0800

    did work

--- 5.1/image_open.py
+++ 5.1/image_open.py
@@ -1,4 +1,33 @@
 from PIL import Image
 
-image_green = Image.open("CS11/5.1/kid-green.jpg").load()
-image_beach = Image.open("CS11/5.1/beach.jpg").load()
+def is_green(r,g,b):
+    if 0 <= r <= 25 and 230 <= g <= 255 and 0 <= b <= 25:
+        return True
+    else:
+        return False
+    
+def colour(r,g,b):
+    if 230 <= r <= 255 and 0 <= g <= 25 and 0 <= b <= 25:
+        return "red"
+    elif 0 <= r <= 25 and 230 <= g <= 255 and 0 <= b <= 25:
+        return "green"
+    elif 0 <= r <= 25 and 0 <= g <= 25 and 230 <= b <= 255:
+        return "blue"
+    elif 230 <= r <= 255 and 230 <= g <= 255 and 230 <= b <= 255:
+        return "white"
+    elif 0 <= r <= 25 and 0 <= g <= 25 and 0 <= b <= 25:
+        return "black"
+    elif 230 <= r <= 255 and 230 <= g <= 255 and 0 <= b <= 25:
+        return "yellow"
+    elif 230 <= r <= 255 and 0 <= g <= 25 and 230 <= b <= 255:
+        return "magenta"
+
+image_green = Image.open("./5.1/kid-green.jpg").load()
+image_beach = Image.open("./5.1/beach.jpg").load()
+
+print(is_green(image_green[0,0][0],image_green[0,0][1],image_green[0,0][2]))
+
+r = int(input("red: "))
+g = int(input("green: "))
+b = int(input("blue: "))
+print(colour(r,g,b))
