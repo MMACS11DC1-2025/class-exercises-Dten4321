4c742e721d542d45ae6e453753137e564327e0db
Dten4321 <shalena.ca@gmail.com>
2025-11-26 12:23:12 -0800

    did work

--- 5.1/binarizer.py
+++ 5.1/binarizer.py
@@ -2,3 +2,23 @@ from PIL import Image
 
 def is_bright(r,g,b):
     return (r + g + b)/3 >= 128
+
+def colour(r,g,b, tolerance):
+    if 255-tolerance <= r <= 255 and 0 <= g <= tolerance and 0 <= b <= tolerance:
+        return "red"
+    elif 0 <= r <= tolerance and 50 <= g <= 255 and 0 <= b <= tolerance:
+        return "green"
+    elif 0 <= r <= tolerance and 0 <= g <= tolerance and 50 <= b <= 255:
+        return "blue"
+    elif 255-tolerance <= r <= 255 and  40 <= g <= 165 and 0 <= b <= tolerance:
+        return "orange"
+    elif 255-tolerance <= r <= 255 and 255-tolerance <= g <= 255 and 0 <= b <= tolerance:
+        return "yellow"
+    elif 255-tolerance <= r <= 255 and 0 <= g <= tolerance and 255-tolerance <= b <= 255:
+        return "pink"
+    elif 255-tolerance <= r <= 255 and 255-tolerance <= g <= 255 and 255-tolerance <= b <= 255:
+        return "white"
+    elif 0 <= r <= tolerance and 0 <= g <= tolerance and 0 <= b <= tolerance:
+        return "black"
+    else:
+        return "unidentified"
