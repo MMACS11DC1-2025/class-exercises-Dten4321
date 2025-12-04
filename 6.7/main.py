0d025de0e05c423980f54530cb130e9358b563a9
Dten4321 <shalena.ca@gmail.com>
2025-12-04 13:16:14 -0800

    Significantly optimized program

--- 6.7/main.py
+++ 6.7/main.py
@@ -3,8 +3,6 @@ import binarizer
 import time
 import random
 
-#Contiguous blob detector
-
 tolerance = 130
 
 image_check_load = Image.open("./6.7/randomMap.png").load()
@@ -25,133 +23,84 @@ colours = {
     "unidentified" : 0
 }
 
-clumps = []
-clumpValue = []
-clumpColour = []
-clumpMatrix = {}
+imgList = [] #data for all pixels in the image
+clumps = [] # each list item will store a "clump" -- a contiguous mass
+clumpValue = [] # which clump is each pixel
+clumpColour = [] # colour of clump
+clumpDisplayColour = [] # display colour of clump
+clumpMatrix = {} # dict, each key is a clump, each key has list of pixels in a group
 maxClumpValue = 0
 
 startTime = time.time()
 
-index = 0
+
 pixelCounter = 0
-print(width*height)
+size = width*height
+print(size)
 input("ready? ")
+
 for x in range(width):
     for y in range(height):
-        r, g, b, a = image_check_load[x,y]
-        
-        colour = ""
-    
-        colour = binarizer.colour(r,g,b, tolerance) 
+        imgList.append(image_check_load[x,y])
+index = 0
+for x in range(width):
+    for y in range(height):
+        #if index % 100000 == 0:
+        #    print("{:.2f}% done, index= {:,}".format((index/size)*100, index))
+        r, g, b, a = imgList[index]
+        colour = binarizer.colour(r,g,b, tolerance)
         colours[colour] += 1
-        programTime = time.time()
+        
         if index > 0 and (height % index) > 0 and index > height:
-            if colour == binarizer.pixelColour(x-1,y, image_check_load, tolerance):
+            if colour == clumpColour[clumpValue[index-height]-1]:
                 clumpValue.append(clumpValue[index-height])
-                clumpMatrix[clumpValue[index-height]].append(index)
-                if colour == binarizer.pixelColour(x,y-1, image_check_load, tolerance):
-                    print("{:.2f}% and {}".format(index/(width*height)*100, pixelCounter))
-                    #eliminatedValue = clumpValue[len(clumpValue)-2]
-                    #for i in range(len(clumpMatrix[eliminatedValue])):
-                    #    clumpValue[i] = clumpValue[index-height]
-                    #    pixelCounter += 1
-            elif colour == binarizer.pixelColour(x,y-1, image_check_load, tolerance):
+                clumpMatrix[clumpValue[index]].append(index)
+            elif colour == clumpColour[clumpValue[index-1]-1]:
                 clumpValue.append(clumpValue[index-1])
+                clumpMatrix[clumpValue[index]].append(index)
             else:
                 maxClumpValue += 1
-                clumpColour.append((random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)))
+                clumpColour.append(colour)
+                clumpDisplayColour.append((random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)))
                 clumpValue.append(maxClumpValue)
                 clumpMatrix[maxClumpValue] = [index]
+        
         elif index < height and index > 0:
-            if colour == binarizer.pixelColour(x,y-1, image_check_load, tolerance):
+            if colour == clumpColour[clumpValue[index-1]-1]:
                 clumpValue.append(clumpValue[index-1])
+                clumpMatrix[clumpValue[index]].append(index)
             else:
-                clumpColour.append((random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)))
                 maxClumpValue += 1
+                clumpColour.append(colour)
+                clumpDisplayColour.append((random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)))
                 clumpValue.append(maxClumpValue)
                 clumpMatrix[maxClumpValue] = [index]
         else:
-            clumpColour.append((int(random.randrange(0, 255)), int(random.randrange(0, 255)), int(random.randrange(0, 255))))
             maxClumpValue += 1
+            clumpColour.append(colour)
+            clumpDisplayColour.append((random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)))
             clumpValue.append(maxClumpValue)
             clumpMatrix[maxClumpValue] = [index]
-        finTime = time.time()
-        #image_output.putpixel((x,y), (clumpColour[clumpValue[index]-1][0], clumpColour[clumpValue[index]-1][1], clumpColour[clumpValue[index]-1][2]))
         index +=1
+print(f"time elpased {time.time() - startTime}")
 
 index = 0
 for x in range(width):
     for y in range(height):
-        r, g, b, a = image_check_load[x,y]
-        colour = binarizer.colour(r,g,b, tolerance) 
+        if index % 5000 == 0:
+            print("{:.2f}% done, index= {:,}".format((index/size)*100, index))
+        r, g, b, a = imgList[index]
+        colour = binarizer.colour(r,g,b, tolerance)
+        colours[colour] += 1
+        
         if index > 0 and (height % index) > 0 and index > height:
-            if colour == binarizer.pixelColour(x-1,y, image_check_load, tolerance):
-                if colour == binarizer.pixelColour(x,y-1, image_check_load, tolerance):
+            if colour == clumpColour[clumpValue[index-height]-1]:
+                if colour == clumpColour[clumpValue[index-1]-1]:
                     correctVaule = index-height
-                    print("{:.2f}% and {:,} pixels".format(index/(width*height)*100, pixelCounter))
                     eliminatedValue = clumpValue[len(clumpValue)-2]
-                    print(len(clumpMatrix[eliminatedValue]))
-                    for i in range(len(clumpMatrix[eliminatedValue])):
+                    for i in range((clumpMatrix[eliminatedValue])):
                         clumpValue[i] = clumpValue[correctVaule]
                         pixelCounter += 1
                     clumpMatrix[correctVaule] += clumpMatrix[eliminatedValue]
                     clumpMatrix[eliminatedValue] = 0
         index +=1
-# TODO finish this!!
-
-index = 0
-for x in range(width):
-    for y in range(height):
-        image_output.putpixel((x,y), (clumpColour[clumpValue[index]-1][0], clumpColour[clumpValue[index]-1][1], clumpColour[clumpValue[index]-1][2]))
-        index +=1
-
-print(len(clumpColour))
-
-print(maxClumpValue)
-    
-total = width * height
-
-endTime = time.time()
-
-print("program took {:.2f} seconds".format(endTime - startTime))
-
-def findpercentage(colour):
-    return (colours[colour]/total) * 100
-
-print("The total percentages are:")
-print("Red at {:.2f}%".format(findpercentage("red")))
-print("Green at {:.2f}%".format(findpercentage("green")))
-print("Indigo at {:.2f}%".format(findpercentage("blue")))
-print("Yellow at {:.2f}%".format(findpercentage("yellow")))
-print("Orange at {:.2f}%".format(findpercentage("orange")))
-print("Black at {:.2f}%".format(findpercentage("black")))
-print("White at {:.2f}%".format(findpercentage("white")))
-print("Pink at {:.2f}%".format(findpercentage("pink")))
-print("Unidentified at {:.2f}%".format(findpercentage("unidentified")))
-
-image_output.show()
-
-index = 0
-seeGroup = int(input("enter group: "))
-for x in range(width):
-    for y in range(height):
-        if clumpValue[index] == seeGroup:
-            image_output.putpixel((x,y), (255, 255, 255))
-        else:
-            image_output.putpixel((x,y), (0, 0, 0))
-        index += 1
-image_output.show()
-
-#index = 0
-#for x in range(width):
-#    for y in range(height):
-#        if clumpValue[index] == seeGroup:
-#            image_output.putpixel((x,y), (255, 255, 255))
-#        else:
-#            image_output.putpixel((x,y), (0, 0, 0))
-#        index += 1
-#image_output.show()
-
-while True:
-    pass
